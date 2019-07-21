6668b742be1817e39e57cebbfc965b749d634159
shashank kumar <kumar0981@gmail.com>
2019-07-21 16:13:18 +0530

    insert,del,update,load example

--- 1-simple-hibernate-app/src/main/java/com/shashank/hibernate/App.java
+++ 1-simple-hibernate-app/src/main/java/com/shashank/hibernate/App.java
@@ -15,8 +15,8 @@ public class App
 	public static void main( String[] args )
 	{
 		Product p= new Product();
-		p.setId(1);
-		p.setName("pen");
+		p.setId(6);
+		p.setName("pen6");
 		
 		SessionFactory sessionFactory = new AnnotationConfiguration().configure("hibernate.cfg.xml").buildSessionFactory();
 		Session session=sessionFactory.openSession();
@@ -25,5 +25,25 @@ public class App
 		session.getTransaction().commit();
 		session.close();
 		sessionFactory.close();
+		
+		SessionFactory sf1=new AnnotationConfiguration().configure("hibernate.cfg.xml").buildSessionFactory();
+		Session s1=sf1.openSession();
+		Product productObj = (Product) s1.load(Product.class, 2);
+		System.out.println("-------------loaded object ----------"+productObj.getId());
+		sf1.close();
+		
+		Session s2=new AnnotationConfiguration().configure().buildSessionFactory().openSession();
+		s2.beginTransaction();
+		Product objToBeDelated = (Product) s2.load(Product.class, 2);
+		s2.delete(objToBeDelated);
+		s2.getTransaction().commit();
+		s2.close();
+		
+		Session s3=new AnnotationConfiguration().configure().buildSessionFactory().openSession();
+		s3.beginTransaction();
+		Product objToBeUpdated = (Product) s3.load(Product.class, 3);
+		objToBeUpdated.setName("paper");
+		s3.getTransaction().commit();
+		s3.close();
 	}
 }
