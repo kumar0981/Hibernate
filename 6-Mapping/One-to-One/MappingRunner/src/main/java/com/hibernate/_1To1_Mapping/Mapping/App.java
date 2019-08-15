58fb0347a5dceafc0c69698b109e5751a3df58b5
shashank kumar <kumar0981@gmail.com>
2019-08-15 13:14:05 +0530

    mapping init

---
+++ 6-Mapping/One-to-One/MappingRunner/src/main/java/com/hibernate/_1To1_Mapping/Mapping/App.java
@@ -0,0 +1,13 @@
+package com.hibernate._1To1_Mapping.Mapping;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
